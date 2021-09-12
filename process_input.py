4c4e418809951a5d3d94dad3748c5af7ccbe494f
Oluwafunmilayo Somoye <32137766+FunmiSomoye@users.noreply.github.com>
2021-09-12 22:03:32 +0100

    Update process_input.py

--- process_input.py
+++ process_input.py
@@ -7,10 +7,10 @@ def process_input(request_data: str):
     """
     asserts that the request data is correct.
     :param request_data: data gotten from the request made to the API
-    :return: the numpy array of the request.data input
+    :return: the values from the dataframe
     """
-    parsed_body = json.loads(request_data)
-    assert len(parsed_body) >= 1 #request_data must be a dictionary (or dictionaries) with 13 features
+    parsed_body = json.loads(request_data)["inputs"]
+    assert len(parsed_body) >= 1 #"'inputs' must be a dictionary (or dictionaries) with 13 features"
     data = pd.DataFrame(parsed_body, columns=["CRIM", 
                                                 "ZN", 
                                                 "INDUS", 
