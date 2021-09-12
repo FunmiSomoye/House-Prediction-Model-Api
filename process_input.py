2f8030a4c3cfb09538c27fdb975aa5a9f670cc0d
Oluwafunmilayo Somoye <32137766+FunmiSomoye@users.noreply.github.com>
2021-09-12 21:35:03 +0100

    Update process_input.py

--- process_input.py
+++ process_input.py
@@ -3,45 +3,27 @@ import pandas as pd
 import json
 
 
-def process_input(request_data: str) -> np.array:
+def process_input(request_data: str):
     """
     asserts that the request data is correct.
     :param request_data: data gotten from the request made to the API
     :return: the numpy array of the request.data input
     """
-    parsed_body = json.loads(request_data)["inputs"]
-    assert len(parsed_body) >= 1 #"'inputs' must be a dictionary with 13 features"
+    parsed_body = json.loads(request_data)
+    assert len(parsed_body) >= 1 #request_data must be a dictionary (or dictionaries) with 13 features
+    data = pd.DataFrame(parsed_body, columns=["CRIM", 
+                                                "ZN", 
+                                                "INDUS", 
+                                                "CHAS", 
+                                                "NOX", 
+                                                "RM", 
+                                                "AGE", 
+                                                "DIS", 
+                                                "RAD", 
+                                                "TAX", 
+                                                "PTRATIO", 
+                                                "B", 
+                                                "LSTAT", 
+                                                "TARGET"])
 
-    data = {"CRIM": [], 
-                     "ZN": [],
-                     "INDUS": [],
-                     "CHAS": [],
-                     "NOX": [],
-                     "RM": [],
-                     "AGE": [],
-                     "DIS": [],
-                     "RAD": [],
-                     "TAX": [],
-                     "PTRATIO": [],
-                     "B": [],
-                     "LSTAT":  [],
-                     "TARGET": []
-                     }
-    
-    for item in range(len(parsed_body)):
-        data["CRIM"].append(parsed_body[item]["CRIM"])
-        data["ZN"].append(parsed_body[item]["ZN"])
-        data["INDUS"].append(parsed_body[item]["INDUS"])
-        data["CHAS"].append(parsed_body[item]["CHAS"])
-        data["NOX"].append(parsed_body[item]["NOX"])
-        data["RM"].append(parsed_body[item]["RM"])
-        data["AGE"].append(parsed_body[item]["AGE"])
-        data["DIS"].append(parsed_body[item]["DIS"])
-        data["RAD"].append(parsed_body[item]["RAD"])
-        data["TAX"].append(parsed_body[item]["TAX"])
-        data["PTRATIO"].append(parsed_body[item]["PTRATIO"])
-        data["B"].append(parsed_body[item]["B"])
-        data["LSTAT"].append(parsed_body[item]["LSTAT"])
-        data["TARGET"].append(parsed_body[item]["TARGET"])
-
-    return pd.DataFrame(data)
+    return data
