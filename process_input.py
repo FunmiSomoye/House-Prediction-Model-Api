b667e45396e3b11ba61cc1d93099f66bf3a1021b
Oluwafunmilayo Somoye <32137766+FunmiSomoye@users.noreply.github.com>
2021-09-12 18:24:37 +0100

    Update process_input.py

--- process_input.py
+++ process_input.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import json
 
 
@@ -8,6 +9,39 @@ def process_input(request_data: str) -> np.array:
     :param request_data: data gotten from the request made to the API
     :return: the numpy array of the request.data input
     """
-    parsed_body = np.asarray(json.loads(request_data)["inputs"])
-    assert len(parsed_body) >= 1 #"'inputs' must be a 1-D array with 13 parameters"
-    return parsed_body
+    parsed_body = json.loads(request_data)["inputs"]
+    assert len(parsed_body) >= 1 #"'inputs' must be a dictionary with 13 features"
+
+    data = {"CRIM": [], 
+                     "ZN": [],
+                     "INDUS": [],
+                     "CHAS": [],
+                     "NOX": [],
+                     "RM": [],
+                     "AGE": [],
+                     "DIS": [],
+                     "RAD": [],
+                     "TAX": [],
+                     "PTRATIO": [],
+                     "B": [],
+                     "LSTAT":  [],
+                     "TARGET": []
+                     }
+    
+    for item in range(len(parsed_body)):
+        data["CRIM"].append(parsed_body[item]["CRIM"])
+        data["ZN"].append(parsed_body[item]["ZN"])
+        data["INDUS"].append(parsed_body[item]["INDUS"])
+        data["CHAS"].append(parsed_body[item]["CHAS"])
+        data["NOX"].append(parsed_body[item]["NOX"])
+        data["RM"].append(parsed_body[item]["RM"])
+        data["AGE"].append(parsed_body[item]["AGE"])
+        data["DIS"].append(parsed_body[item]["DIS"])
+        data["RAD"].append(parsed_body[item]["RAD"])
+        data["TAX"].append(parsed_body[item]["TAX"])
+        data["PTRATIO"].append(parsed_body[item]["PTRATIO"])
+        data["B"].append(parsed_body[item]["B"])
+        data["LSTAT"].append(parsed_body[item]["LSTAT"])
+        data["TARGET"].append(parsed_body[item]["TARGET"])
+
+    return pd.DataFrame(data)
